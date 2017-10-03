9404db615098f89c3d4bb4e639985526152eeba3
minikin <djminikin@gmail.com>
2017-10-03 10:41:27 +0200

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class BaiduMeetsReactNative extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('BaiduMeetsReactNative', () => BaiduMeetsReactNative);
