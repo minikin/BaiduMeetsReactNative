d4d974554e6c05a229eff356f9b00cd43bb8013e
minikin <djminikin@gmail.com>
2017-10-04 07:00:41 +0200

    Add baidu sdk to android project

---
+++ android/app/src/main/java/com/baidumeetsreactnative/BaiduMapModule.java
@@ -0,0 +1,16 @@
+package com.baidumeetsreactnative;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+public class BaiduMapModule extends BaseModule {
+
+    private static final String REACT_CLASS = "BaiduMapModule";
+    public BaiduMapModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        context = reactContext;
+    }
+
+    public String getName() {
+        return REACT_CLASS;
+    }
+}
