9cf5a3fd4577c63469bd5211cc989fc6060357ce
minikin <djminikin@gmail.com>
2017-10-19 15:17:55 +0200

    Fix bug when pin image is nil

--- ios/BaiduBridgeHelper.m
+++ ios/BaiduBridgeHelper.m
@@ -15,18 +15,20 @@
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(setPinImageWith:(NSString *)imageName) {
-  UIImage *image = [UIImage imageWithContentsOfFile: imageName];
-  NSDictionary *userInfo = [NSDictionary dictionaryWithObject:image forKey:pinImageKey];
-  [[NSNotificationCenter defaultCenter] postNotificationName:sendCustomPinImage object:self userInfo:userInfo];
+    UIImage *image = [UIImage imageWithContentsOfFile: imageName];
+    RCTLogInfo(@"======== JS NEWS ======== %@",image.description);
+    if (image != nil) {
+      NSDictionary *userInfo = [NSDictionary dictionaryWithObject:image forKey:pinImageKey];
+      [[NSNotificationCenter defaultCenter] postNotificationName:sendCustomPinImage object:self userInfo:userInfo];
+    }
 }
 
 RCT_EXPORT_METHOD(setCustomMapStyleWith:(NSString *)JSONFile) {
-  NSDictionary *userInfo = [NSDictionary dictionaryWithObject:JSONFile forKey:jsonFileKey];
-  [[NSNotificationCenter defaultCenter] postNotificationName: customMapStyleWithJSON object:self userInfo: userInfo];
+    NSDictionary *userInfo = [NSDictionary dictionaryWithObject:JSONFile forKey:jsonFileKey];
+    [[NSNotificationCenter defaultCenter] postNotificationName: customMapStyleWithJSON object:self userInfo: userInfo];
 }
 
 RCT_EXPORT_METHOD(customMapStyle:(BOOL)enable) {
-  RCTLogInfo(@"======== JS NEWS ======== %d",enable);
   NSDictionary *userInfo = [NSDictionary dictionaryWithObject: [NSNumber numberWithBool:enable]  forKey: customMapKey];
   [[NSNotificationCenter defaultCenter] postNotificationName: useCustomMapStyle object:self userInfo: userInfo];
 }
